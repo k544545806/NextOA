2a77f40ebe60d8f433b9cb8400967b18135d8961
Joe <544545806@qq.com>
2015-01-13 00:23:41 +0800

    no message

---
+++ src/org/NextOA/NextOA.java
@@ -0,0 +1,12 @@
+package org.NextOA;
+
+
+/**
+ * 前段控制器
+ * 
+ * @author Joe
+ * @version $Id: NextOA.java,v 1.0 2015/1/13 
+ * */
+public class NextOA {
+
+}
--- src/org/NextOA/a.java
+++
@@ -1,5 +0,0 @@
-package org.NextOA;
-
-public class a {
-
-}
